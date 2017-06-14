b06b47abf57dcc7d691b5e1b0d2e3ba15d7409a6
tolgacanunal <tesleax@outlook.com>
2017-06-14 10:03:26 +0300

    new String

--- app/src/main/java/hipo/com/customviewexercise/view/AnswerView.java
+++ app/src/main/java/hipo/com/customviewexercise/view/AnswerView.java
@@ -63,6 +63,7 @@ public class AnswerView extends LinearLayout {
                     letter = "D";
                     break;
             }
+            String NEW = "new String";
             String description = "Description of the selection " + letter;
             tvSelectionDescription.setText(description);
             letter += " )";
